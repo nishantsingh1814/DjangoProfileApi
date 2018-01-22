d3d94d54b7f2289234b5a6d851b8184ad85cd57e
Nishant <nishant@Praksheps-MacBook-Air-5.local>
2018-01-22 18:27:03 +0530

    added first viewset

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
@@ -66,3 +67,18 @@ class HelloApiView(APIView):
 
 
         return Response({'method':'delete'})
+
+
+class HelloViewSet(viewsets.ViewSet):
+    """Test API ViewSet"""
+
+    def list(self,request):
+        """Return a hello message."""
+
+        a_viewset=[
+            'Uses actions (list,create,retrieve,update,partial_update)',
+            'Automatically maps to URLs using Routers',
+            'Provides more functionality with less code'
+        ]
+
+        return Response({'message':'Hello!','a_viewset':a_viewset})
